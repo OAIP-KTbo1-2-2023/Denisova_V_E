2c3336a3afd946e4a5fa7e55cf9ad7c6f43f8b7b
Ника Карась <nika.karas135@gmail.com>
2023-12-07 08:15:28 +0300

    + 2,3

---
+++ 2l/lab2.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");
+
+    float array[100];
+    int n=0;
+    float sum=0, kol = 0.0, res;
+
+    do {
+        printf("Введите количество чисел в массиве, не больше 100 элементов: ");
+        scanf_s("%i", &n);
+    } while (n <= 0);
+
+
+    // Цикл для заполнения массива значениями
+    for (int i = 0; i < n; i++) {
+        printf("Введите %i элемент массива: ", i + 1);
+        scanf_s("%f", &array[i]);
+    }
+
+    for (int i = 1; i < n; i += 2) {
+        sum = sum + array[i];
+        kol++;
+    };
+    res = sum / kol;
+    printf("%f", res);
+}
---
+++ lab3/vs/lab3333.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <Windows.h>
+int main(void)
+{
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	setlocale(LC_ALL, "RUS");
+	char str[300], p;
+	int i, k = 0;
+
+	//printf("Введите строку: ");
+	gets_s(str);
+	//printf("Вот Ваша строка: %s", str);
+
+	for (i = 0; i <= 300; i++) {
+		if ((str[i] == ' ') || (str[i] == 0)) {
+			p = str[i - 1];
+
+			str[i - 1] = str[i - k];
+			str[i - k] = p;
+			k = 0;
+		}
+		else {
+			k++;
+		}
+	}
+	printf("%s", str);
+	return 0;
+
+}
